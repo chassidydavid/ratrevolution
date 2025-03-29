6c8823658328d4eb4ae559ca2ed6a62563353dc6
chassidydavid <chassidydavid@gmail.com>
2025-03-28 20:14:54 -0400

    CHANGES TO HTML/CSS/JS

--- javascript.js
+++ javascript.js
@@ -26,10 +26,34 @@ button.addEventListener('mouseout', function() {
   sound2.currentTime = 0; // Reset to the beginning
 });
 
+const displayArt = document.getElementById("displayArt")
+convertToAsciiWithSpacing(displayArt)
 
 function startGame() {
 	console.log("welcome to the subway tunnels. what's the password? (say it with your chest) \n ====================================================================================================.\r\nS |   |     |       | |             |   |           |   |             |   |                 |   |   |\r\n| | | | .=. \'== .== | | .== .=====. | | \'======== | | | | ==========. | | | .=============. | | \'=. |\r\n|   | | | |     |   | | |   |     |   |     |     |   | |     |     |   |   |     |       |   |   | |\r\n|===| | | |=====| ==| | | .=| .== \'======== | .=======| \'==== |==== \'===+===| ==. | .=====+=====. | |\r\n|   |   | |     |   |   | | | |     |       | |       |   |   |         |   |   |   |     |     |   |\r\n| ======| | | .==== | .=\' | | | ====\' .=====| | .=======. | .=\' .=======| | | | |===| ==. | .=. \'=. |\r\n|       |   | |     | |   | | |   |   |     | | |   |   |   |   |       | |   | |   |   |   | |   | |\r\n| ====. | ==| | ======\' .=\' | \'=. | ===== | | | | | | | \'===\' ==\' ====. | \'===| | | \'== \'===\' \'=. | |\r\n|     |     |           |       |         |   |   |   |               |       |   |             |   E\r\n\'==================================================================================================== \n _..----.._    _\r\n            .\'  .--.    \"-.(0)_\r\n\'-.__.-\'\"\'=:|   ,  _)_ \\__ . c\\\'-..\r\n             \'\'\'------\'---\'\'---\'-\"");
 	setTimeout(password, 8000); //8 seconds
+	displayArt.innerHTML = "         __             _,-\"~^\"-.\r\n       _\/\/ )      _,-\"~`         `.\r\n     .\" ( \/`\"-,-\"`                 ;\r\n    \/ 6                             ;\r\n   \/           ,             ,-\"     ;\r\n  (,__.--.      \\           \/        ;\r\n   \/\/\'   \/`-.\\   |          |        `._________\r\n     _.-\'_\/`  )  )--...,,,___\\     \\-----------,)\r\n   (((\"~` _.-\'.-\'           __`-.   )         \/\/\r\n         (((\"`             (((---~\"`         \/\/\r\n                                            ((________________\r\n                                            `----\"\"\"\"~~~~^^^```"
+convertToAsciiWithSpacing(displayArt)
+}
+
+function convertToAsciiWithSpacing(element) {
+  if (!element) {
+    return;
+  }
+
+  let html = element.innerHTML;
+
+  // Replace < and > with their HTML entities to prevent them from being interpreted as HTML tags
+  html = html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+  // Replace spaces with &nbsp; to preserve them
+  html = html.replace(/ /g, "&nbsp;");
+
+  // Replace new lines with <br> tags for line breaks
+  html = html.replace(/\n/g, "<br>");
+
+  // Set the innerHTML of the element to the modified HTML
+  element.innerHTML = html;
 }
 
 function password(){
